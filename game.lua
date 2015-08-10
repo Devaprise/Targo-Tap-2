fb3f1521dfd1f1bebeb49f4679c1af79e0c477ab
VirusKA <devaprise@outlook.com>
2015-08-10 14:21:18 -0400

    first working version of TT
    Finally have completed most of gameplay. All that’s left is number
    shuffling then we can squash bugs and work on aesthetics.

--- game.lua
+++ game.lua
@@ -21,55 +21,6 @@ local Btn2
 local Btn3
 local Btn4
 
-local numberText = display.newText( 0, display.contentCenterX, display.contentCenterY+45, native.systemFont, 100 )
-numberText:setFillColor(0, 0, 0)
-numberText.text = ( math.random(1, 4) )
-
-local scoreNumber = display.newText( 0, display.contentWidth/1.7, display.contentHeight/4.5, native.systemFont, 60 )
-scoreNumber:setFillColor(.27, .651, 60)
-
-local function onBtnRelease()
-	-- go to game.lua scene
-	print( "You pressed Button!" )
-    numberText.text = ( math.random(1, 4) )
-	return true	-- indicates successful touch
-end
-local function check()
-    if Btn1:getLabel() == numberText.text then
-        print("You won Da prize boii!")
-        return true
-    else
-        print("sike!! that's ze wrong numero!")
-        return false
-    end
-end
-local function check2()
-    if Btn2:getLabel() == numberText.text then
-        print("You won Da prize boii!")
-        return true
-    else
-        print("sike!! that's ze wrong numero!")
-        return false
-    end
-end
-local function check3()
-    if Btn3:getLabel() == numberText.text then
-        print("You won Da prize boii!")
-        return true
-    else
-        print("sike!! that's ze wrong numero!")
-        return false
-    end
-end
-local function check4()
-    if Btn4:getLabel() == numberText.text then
-        print("You won Da prize boii!")
-        return true
-    else
-        print("sike!! that's ze wrong numero!")
-        return false
-    end
-end
 function scene:create( event )
 
 	-- Called when the scene's view does not exist.
@@ -83,7 +34,59 @@ function scene:create( event )
     timeText:setFillColor(.27, .651, 60)
     local scoreText = display.newText( "SCORE:", display.contentWidth/2.3, display.contentHeight/4.5, native.systemFont, 40 )
     scoreText:setFillColor(.27, .651, 60)
-    
+    local numberText = display.newText( 0, display.contentCenterX, display.contentCenterY+45, native.systemFont, 100 )
+    numberText:setFillColor(0, 0, 0)
+    numberText.text = ( math.random(1, 4) )
+
+    local scoreNumber = display.newText( 0, display.contentWidth/1.7, display.contentHeight/4.5, native.systemFont, 60 )
+    scoreNumber:setFillColor(.27, .651, 60)
+
+    local function onBtnRelease()
+        -- go to game.lua scene
+        print( "You pressed Button!" )
+        numberText.text = ( math.random(1, 4) )
+        return true	-- indicates successful touch
+    end
+    local function check()
+        if Btn1:getLabel() == numberText.text then
+            print("You won Da prize boii!")
+            scoreNumber.text = scoreNumber.text + 1
+            return true
+        else
+            print("sike!! that's ze wrong numero!")
+            return false
+        end
+    end
+    local function check2()
+        if Btn2:getLabel() == numberText.text then
+            print("You won Da prize boii!")
+            scoreNumber.text = scoreNumber.text + 1
+            return true
+        else
+            print("sike!! that's ze wrong numero!")
+            return false
+        end
+    end
+    local function check3()
+        if Btn3:getLabel() == numberText.text then
+            print("You won Da prize boii!")
+            scoreNumber.text = scoreNumber.text + 1
+            return true
+        else
+            print("sike!! that's ze wrong numero!")
+            return false
+        end
+    end
+    local function check4()
+        if Btn4:getLabel() == numberText.text then
+            print("You won Da prize boii!")
+            scoreNumber.text = scoreNumber.text + 1
+            return true
+        else
+            print("sike!! that's ze wrong numero!")
+            return false
+        end
+    end
     Btn1 = widget.newButton {
         label = "1",
         onPress = check,
@@ -174,25 +177,25 @@ function scene:show( event )
 		-- INSERT code here to make the scene come alive
 		-- e.g. start timers, begin animation, play audio, etc.
         
---        local timeLimit = 20
---        timeLeft = display.newText(timeLimit, display.contentWidth/1.7, display.contentHeight/6, native.systemFontBold, 65)
---        timeLeft:setFillColor(.27, .651, 60)
---
---        local function timerDown()
---           timeLimit = timeLimit-1
---           timeLeft.text = timeLimit
---                
---            if(timeLimit==10)then
---                timeLeft:setFillColor(255,0,0)
---            elseif(timeLimit==0)then
---                print("Time Out") -- or do your code for time out
---                composer.gotoScene( "gameover", "fade", 250 )
---            end
---          end
---        timer.performWithDelay(1000,timerDown,timeLimit)
---        
---        
---        sceneGroup:insert( timeLeft )
+        local timeLimit = 20
+        timeLeft = display.newText(timeLimit, display.contentWidth/1.7, display.contentHeight/6, native.systemFontBold, 65)
+        timeLeft:setFillColor(.27, .651, 60)
+
+        local function timerDown()
+           timeLimit = timeLimit-1
+           timeLeft.text = timeLimit
+                
+            if(timeLimit==10)then
+                timeLeft:setFillColor(255,0,0)
+            elseif(timeLimit==0)then
+                print("Time Out") -- or do your code for time out
+                composer.gotoScene( "gameover", "fade", 250 )
+            end
+          end
+        timer.performWithDelay(1000,timerDown,timeLimit)
+        
+        
+        sceneGroup:insert( timeLeft )
 	end
 end
 
@@ -236,10 +239,10 @@ function scene:destroy( event )
         Btn4:removeSelf()
         Btn4 = nil
     end
---    if timerDown then
---        timerDown:removeSelf()
---        timerDown = nil
---    end
+    if timerDown then
+        timerDown:removeSelf()
+        timerDown = nil
+    end
 end
 
 ---------------------------------------------------------------------------------
