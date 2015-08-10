805a9745569aabb03ae3a2eaa8e3a9cf40dc3dd8
VirusKA <devaprise@outlook.com>
2015-08-10 14:08:15 -0400

    initial work on game logic
    Finally got the code of the game logic working. Basically, I used four
    functions to check the text to find a match then prints in the console
    a response. If they don’t match, the console produces a different
    response. Took way to long to figure out a simple solution. I can now
    work on scoring and we’ll have the first functional version of TT!

--- game.lua
+++ game.lua
@@ -21,9 +21,12 @@ local Btn2
 local Btn3
 local Btn4
 
-local numberText = display.newText( 0, display.contentCenterX, display.contentCenterY+50, native.systemFont, 100 )
+local numberText = display.newText( 0, display.contentCenterX, display.contentCenterY+45, native.systemFont, 100 )
 numberText:setFillColor(0, 0, 0)
 numberText.text = ( math.random(1, 4) )
+
+local scoreNumber = display.newText( 0, display.contentWidth/1.7, display.contentHeight/4.5, native.systemFont, 60 )
+scoreNumber:setFillColor(.27, .651, 60)
 
 local function onBtnRelease()
 	-- go to game.lua scene
@@ -31,7 +34,42 @@ local function onBtnRelease()
     numberText.text = ( math.random(1, 4) )
 	return true	-- indicates successful touch
 end
-
+local function check()
+    if Btn1:getLabel() == numberText.text then
+        print("You won Da prize boii!")
+        return true
+    else
+        print("sike!! that's ze wrong numero!")
+        return false
+    end
+end
+local function check2()
+    if Btn2:getLabel() == numberText.text then
+        print("You won Da prize boii!")
+        return true
+    else
+        print("sike!! that's ze wrong numero!")
+        return false
+    end
+end
+local function check3()
+    if Btn3:getLabel() == numberText.text then
+        print("You won Da prize boii!")
+        return true
+    else
+        print("sike!! that's ze wrong numero!")
+        return false
+    end
+end
+local function check4()
+    if Btn4:getLabel() == numberText.text then
+        print("You won Da prize boii!")
+        return true
+    else
+        print("sike!! that's ze wrong numero!")
+        return false
+    end
+end
 function scene:create( event )
 
 	-- Called when the scene's view does not exist.
@@ -48,6 +86,7 @@ function scene:create( event )
     
     Btn1 = widget.newButton {
         label = "1",
+        onPress = check,
         onRelease = onBtnRelease,
         --properties for a rectangle button...
         shape = "Rect",
@@ -60,6 +99,7 @@ function scene:create( event )
     }
     Btn2 = widget.newButton {
         label = "2",
+        onPress = check2,
         onRelease = onBtnRelease,
         --properties for a rectangle button...
         shape = "Rect",
@@ -72,6 +112,7 @@ function scene:create( event )
     }
     Btn3 = widget.newButton {
         label = "3",
+        onPress = check3,
         onRelease = onBtnRelease,
         --properties for a rectangle button...
         shape = "Rect",
@@ -84,6 +125,7 @@ function scene:create( event )
     }
     Btn4 = widget.newButton {
         label = "4",
+        onPress = check4,
         onRelease = onBtnRelease,
         --properties for a rectangle button...
         shape = "Rect",
@@ -107,9 +149,10 @@ function scene:create( event )
     
     Btn4.x = display.contentCenterX + (display.contentWidth/4);
     Btn4.y = display.contentCenterY + (display.contentHeight/6);
-    
+            
     sceneGroup:insert( timeText )
     sceneGroup:insert( scoreText )
+    sceneGroup:insert( scoreNumber )
     sceneGroup:insert( Btn1 )
     sceneGroup:insert( Btn2 )
     sceneGroup:insert( Btn3 )
@@ -130,25 +173,26 @@ function scene:show( event )
 		-- 
 		-- INSERT code here to make the scene come alive
 		-- e.g. start timers, begin animation, play audio, etc.
-        local timeLimit = 20
-        timeLeft = display.newText(timeLimit, display.contentWidth/1.7, display.contentHeight/6, native.systemFontBold, 65)
-        timeLeft:setFillColor(.27, .651, 60)
-
-        local function timerDown()
-           timeLimit = timeLimit-1
-           timeLeft.text = timeLimit
-                
-            if(timeLimit==10)then
-                timeLeft:setFillColor(255,0,0)
-            elseif(timeLimit==0)then
-                print("Time Out") -- or do your code for time out
-                composer.gotoScene( "gameover", "fade", 250 )
-            end
-          end
-        timer.performWithDelay(1000,timerDown,timeLimit)
         
-        
-        sceneGroup:insert( timeLeft )
+--        local timeLimit = 20
+--        timeLeft = display.newText(timeLimit, display.contentWidth/1.7, display.contentHeight/6, native.systemFontBold, 65)
+--        timeLeft:setFillColor(.27, .651, 60)
+--
+--        local function timerDown()
+--           timeLimit = timeLimit-1
+--           timeLeft.text = timeLimit
+--                
+--            if(timeLimit==10)then
+--                timeLeft:setFillColor(255,0,0)
+--            elseif(timeLimit==0)then
+--                print("Time Out") -- or do your code for time out
+--                composer.gotoScene( "gameover", "fade", 250 )
+--            end
+--          end
+--        timer.performWithDelay(1000,timerDown,timeLimit)
+--        
+--        
+--        sceneGroup:insert( timeLeft )
 	end
 end
 
@@ -192,6 +236,10 @@ function scene:destroy( event )
         Btn4:removeSelf()
         Btn4 = nil
     end
+--    if timerDown then
+--        timerDown:removeSelf()
+--        timerDown = nil
+--    end
 end
 
 ---------------------------------------------------------------------------------
